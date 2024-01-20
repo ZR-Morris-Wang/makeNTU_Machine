534938c0e347e5e4327f7dbbc6aeb27e5bd42757
YBA857142 <grcgs2601@gmail.com>
2024-01-20 14:56:48 +0800

    deal with migration

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,36 @@
-// //for testing
-// import { PrismaClient } from '@prisma/client'
-
-// const prisma = new PrismaClient()
-
-// async function main() {
-//     await prisma.request.create({
-//         data: {
-//             group:1,
-//             type:"3dp",
-//             number:1,
-//             filename:"000",
-//             comment:"hi",
-//             status:"lol"
-//         },
-//       })
-//     const allUsers = await prisma.request.findMany()
-//     console.log(allUsers)
-// }
+//for testing
+'use client'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    // await prisma.laserCutReq.create({
+    //     data: {
+    //         group:1,
+    //         type:"3dp",
+    //         number:1,
+    //         filename:"000",
+    //         comment:"hi",
+    //         status:"lol"
+    //     },
+    //   })
+    // const allUsers = await prisma.request.findMany()
+    // console.log(allUsers)
+    await prisma.account.create({
+        data:{
+            name:"testhaha",
+            password:"testhaha",
+            permission:"admin"
+        },
+        })
+}
   
-// main()
-// .then(async () => {
-//     await prisma.$disconnect()
-// })
-// .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-// })
+main()
+.then(async () => {
+    await prisma.$disconnect()
+})
+.catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
   
