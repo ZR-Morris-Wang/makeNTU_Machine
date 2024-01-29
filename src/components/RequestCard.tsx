81d68ec6c7a1ff4e6ce4db80198938b39bb46c03
YBA857142 <grcgs2601@gmail.com>
2024-01-30 01:28:38 +0800

    queuelist finished

--- src/components/RequestCard.tsx
+++ src/components/RequestCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Tab, TableCell } from "@mui/material";
+import TableRow from "@mui/material";
 
 export type RequestCardProps = {
     information: {
@@ -13,9 +15,7 @@ export type RequestCardProps = {
 
 export default function RequestCard({ information, isSender }: RequestCardProps) {
     const materialList = ["3mm密集板","5mm密集板","3mm壓克力","5mm壓克力"]
-    return (
-        <>
-        <div
+            {/* <div
             className={`g-4 w-full h-12 flex items-center justify-between border-black border-b-2 ${
                 isSender ? "bg-yellow-200" : ""
             }`}
@@ -25,7 +25,16 @@ export default function RequestCard({ information, isSender }: RequestCardProps)
             <p className="text-lg font-bold">{materialList[information?.material[1]]}</p>
             <p className="text-lg font-bold">{information?.status}</p>
             <p className="text-lg font-bold">{information?.comment}</p>
-        </div>
+        </div> */}
+    return (
+        <>
+            <TableRow>
+                <TableCell>{information?.group}</TableCell>
+                <TableCell>{information?.filename}</TableCell>
+                <TableCell>{materialList[information?.material[1]]}</TableCell>
+                <TableCell>{information?.status}</TableCell>
+                <TableCell>{information?.comment}</TableCell>
+            </TableRow>
         </>
     )
 }
