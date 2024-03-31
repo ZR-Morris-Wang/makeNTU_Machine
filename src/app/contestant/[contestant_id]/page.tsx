be3c5526745d47bc0b767bf54bc84ae4b3907029
ZR-Morris-Wang <ppmmlzr@gmail.com>
2024-03-31 16:49:26 +0800

    authentication test

--- src/app/contestant/[contestant_id]/page.tsx
+++ src/app/contestant/[contestant_id]/page.tsx
@@ -4,11 +4,40 @@ import { useRouter, usePathname } from "next/navigation";
 import LaserCutQueueListForContestant from "@/components/LaserCutQueueListForContestant";
 import ThreeDPQueueListForContestant from "@/components/ThreeDPQueueListForContestant"
 import Map from "@/components/Map";
+import jwt from "jsonwebtoken";
+import { env } from "../../../utils/env";
+import { decode } from "punycode";
 
 export default function contestant() {
     const router = useRouter();
     const pathname = usePathname();
-    console.log();
+    const secretkey : string = process.env.PASSWORD_SECRET ? process.env.PASSWORD_SECRET : "Secret";
+    const token = localStorage.getItem("token");
+    
+    function decodeJWT(token: string): Record<string, any> | null {
+        const parts = token.split('.');
+        if (parts.length !== 3) {
+            return null; // Invalid JWT format
+        }
+    
+        const payload = Buffer.from(parts[1], 'base64').toString('utf-8');
+        return JSON.parse(payload);
+    }
+    
+
+    
+    if (!token) {
+        alert("You are not logged in.");
+        router.push("/login");
+    } else {
+        const decodedPayload = decodeJWT(token);
+        const permission = decodedPayload?.permission;
+        console.log(permission);
+        if(!permission || permission !== "contestant") {
+            router.push("/login");
+        }
+    }
+
     return (
         <>
         <Map />
